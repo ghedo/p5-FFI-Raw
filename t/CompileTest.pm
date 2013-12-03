cbbcff2de9c6387cda1b53de85ac95180ea026af
Alessandro Ghedini <alessandro@ghedini.me>
2013-12-03 17:05:37 +0100

    CompileTest.pm: explicitly set C standard (again)

--- t/CompileTest.pm
+++ t/CompileTest.pm
@@ -10,7 +10,10 @@ sub compile {
 
 	my $b = ExtUtils::CBuilder -> new;
 
-	my $obj_file = $b -> compile(source => $src_file);
+	my $obj_file = $b -> compile(
+		source => $src_file,
+		extra_compiler_flags => '-std=gnu99'
+	);
 	my $lib_file = $b -> link(objects => $obj_file);
 
 	return $lib_file;
