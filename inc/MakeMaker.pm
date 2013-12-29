786ed1b9fbd10ee856b45d02706342d75ab33c28
Graham Ollis <perl@wdlabs.com>
2013-12-29 10:15:23 -0500

    build libffi under MSWin32 64bit tests still fail.

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -8,10 +8,15 @@ override _build_MakeFile_PL_template => sub {
 	my ($self) = @_;
 
 	my $template  = <<'TEMPLATE';
+
+use Config;
+
 sub MY::postamble {
-  if ($^O eq 'MSWin32') {
-    return "\t$^X -MAlien::MSYS=msys_run -e \"chdir 'xs/libffi'; msys_run 'sh configure MAKEILFO=true --disable-builddir --with-pic'; msys_run 'make'\"\n\n";
-  }
+	if ($^O eq 'MSWin32') {
+		my $configure_args = 'MAKEILFO=true --disable-builddir --with-pic';
+		$configure_args .= ' --build=x86_64-pc-mingw64' if $Config{archname} =~ /^MSWin32-x64/;
+		return "\t$^X -MAlien::MSYS=msys_run -e \"chdir 'xs/libffi'; msys_run 'sh configure $configure_args'; msys_run 'make'\"\n\n";
+	}
   return <<'MAKE_LIBFFI';
 $(MYEXTLIB):
 	cd xs/libffi && ./configure MAKEINFO=true --disable-builddir --with-pic && $(MAKE)
