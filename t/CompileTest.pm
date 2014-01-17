495e5aacfacf1a05016ee4c31f0d36ae5f2def80
Graham Ollis <plicease@cpan.org>
2014-01-17 12:20:59 -0500

    provide include directory as a listref

--- t/CompileTest.pm
+++ t/CompileTest.pm
@@ -17,7 +17,7 @@ sub compile {
 	my $obj_file = $b -> compile(
 		source => $src_file,
 		extra_compiler_flags => $^O eq 'MSWin32' && $Config{cc} =~ /cl(\.exe)?$/ ? '' : '-std=gnu99',
-		include_dirs => $FindBin::Bin,
+		include_dirs => [$FindBin::Bin],
 	);
 
 	return $b -> link(objects => $obj_file)
