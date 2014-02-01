0e38b6a25a5bd587570eadb886efa6d5c4115586
Alessandro Ghedini <alessandro@ghedini.me>
2014-02-01 12:40:32 +0100

    Raw.pm: document $library = undef behaviour

--- lib/FFI/Raw.pm
+++ lib/FFI/Raw.pm
@@ -53,7 +53,9 @@ Note that this module has nothing to do with L<FFI>.
 Create a new C<FFI::Raw> object. It loads C<$library>, finds the function
 C<$function> with return type C<$return_type> and creates a calling interface.
 
-This function takes also a variable number of types, representing the argument
+If <$library> is C<undef> then the function is searched in the main program.
+
+This function also takes a variable number of types, representing the argument
 of the wanted function.
 
 =head2 new_from_ptr( $function_ptr, $return_type [, $arg_type ...] )
